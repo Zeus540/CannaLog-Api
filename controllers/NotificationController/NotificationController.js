bf18cf844bb884dfa9a3b459b5b0d06dc05c2306
Zeus540 <zaheerroberts4@gmail.com>
2023-10-17 11:14:52 +0200

    updated notifications

--- controllers/NotificationController/NotificationController.js
+++ controllers/NotificationController/NotificationController.js
@@ -29,7 +29,7 @@ module.exports = {
   JOIN 
       notification_actions ON user_notifications.notification_action_id = notification_actions.notification_action_id
   LEFT JOIN 
-      users AS actors ON user_notifications.actor_user_id = actors.user_id -- Join with the users table for actor_user_id
+      users AS actors ON user_notifications.actor_user_id = actors.user_id 
   WHERE 
       user_notifications.user_id = ?
       ORDER BY creation_date DESC;
@@ -38,15 +38,62 @@ module.exports = {
   
       db.query(get_notifications, [req.user.user_id], (err, result, fields) => {
         if (err) {
-          console.log(err)
-  
+          res.status(500).json({ error: 'Failed to get notification' });
         } else {
-  
           res.send(result)
-  
         }
       })
     },
+    read:(req, res)=>{
+      /* ...
+            // #swagger.tags = ['Notifications']
+            ...
+            */
+       
+          let id = req.params.notification_id
+    
+          console.log("id",id)
+          const read_notifications = `
+          UPDATE user_notifications
+          SET user_notifications.notification_read = ?
+          WHERE user_notifications.user_notification_id = ?;
+        `;
+    
+     
+        db.query(read_notifications, [1, id], (err, result, fields) => {
+          if (err) {
+            res.status(500).json({ error: 'Failed to update entry' });
+          } else {
+            res.status(200).json({ message: 'Notification successfully read'});
+          }
+        });
+    
+        },
+    readAll:(req, res)=>{
+  /* ...
+        // #swagger.tags = ['Notifications']
+        ...
+        */
+   
+      let ids = req.body.ids
+
+      const read_notifications = `
+      UPDATE user_notifications
+      SET user_notifications.notification_read = ?
+      WHERE user_notifications.user_notification_id IN (?);
+    `;
+
+ 
+    db.query(read_notifications, [1, ids], (err, result, fields) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Failed to update entries' });
+      } else {
+        res.status(200).json({ message: 'Entries updated successfully' });
+      }
+    });
+
+    }
   }
   
   
--- routes/notifications.js
+++ routes/notifications.js
@@ -4,5 +4,6 @@ const authenticateToken = require('../middleware/authenticate')
 const NotificationController = require('../controllers/NotificationController/NotificationController')
 
 router.get('/notifications',authenticateToken, NotificationController.get)
-
+router.patch('/notifications/read_all',authenticateToken, NotificationController.readAll)
+router.post('/notifications/:notification_id',authenticateToken, NotificationController.read)
 module.exports = router;

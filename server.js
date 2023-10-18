36f5c13a914fdde82b995ddd13f5476419c077d2
Zeus540 <zaheerroberts4@gmail.com>
2023-10-18 16:57:27 +0200

    updated notification sound trigger

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(router)
 app.set('json spaces', 2)
